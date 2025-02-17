82e74d0c6dd88b41cc04e0ab01210b2bc9065e4c
vickymalindo <vicky.malindo@gmail.com>
2025-02-17 15:43:06 +0700

    feat: add user entity

---
+++ src/typeorm/entities/User.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// this entity called users or the table name is users
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  authStrategy: string;
+}
